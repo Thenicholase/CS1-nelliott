5cb7042b102f7d94b04f5aaf1d603f90c861d288
Nicholas Elliott <naelliott@mavs.coloradomesa.edu>
2023-02-10 09:26:00 -0700

    Fixed all

--- labs/ASCII/main.cpp
+++ labs/ASCII/main.cpp
@@ -18,19 +18,31 @@ using namespace std; //resolve cout, cin, and endl names
  
 //main entry point of the program
 int main()
+
+
 {
+    string firstName; 
     //FIXME3: declare a variable to store name
-    
+
     //FIXME4: prompt user to enter their name and store the value in variable
-    
+    cout << " Enter name: ";
+    cin >> firstName; 
+    cout << " Nice meeting you, " << firstName << "!" << endl; 
     //FIXME5: greet the name using the variable as the following output
     //must output: Nice meeting you, <name>!
 
-    cout << "Hope you like my Tom & Jerry ASCII art...\n\n";
+    ;cout << "My ASCII Art...\n\n";
 
-    string line1 = "  |\\_/|   **********************    (\\_/)\n";
+    string line1 = "                        |\\_/|   **********************    (\\_/)\n";
     cout << line1;
-
+    string line2 = " /  @  @ \\      	*                  ASCII Lab               *  	  (='.'=)\n";
+    cout << line2;
+    string line3 = " ( >   0  <   )       *          By: <Nicholas Elliott>      * 	( \" )_( \" )\n";
+    cout << line3;
+    string line4 = "      >>x<<              *            CSCI 111              *          >>x<<\n";
+    cout << line4;
+    string line5 = "      /   O  \\       	     *****************************             /  O  \\\n";
+    cout << line5;
     //FIXME6: use variable to print the second line line of the graphic
     //FIXME7: print the third line of the graphics
     //FIXME8: use variable to print the fourth line
