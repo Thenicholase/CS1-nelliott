b63c7e91dedc52d06683bcf9b635d4171488452c
Nicholas Elliott <naelliott@mavs.coloradomesa.edu>
2023-02-13 11:16:58 -0700

    Completed Stage 0

---
+++ Repositories/Assignments/Stdio/main.cpp
@@ -0,0 +1,30 @@
+// Name: Nicholas Elliott
+// Section CSCI 111-001
+// Date: 02/13/23
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () 
+{
+    string firstName;
+    cout << "Hey there, what's your name?" << endl;
+    cin >> firstName;
+
+    cout << "Hey," << firstName << "!" << endl;
+    cout << "The hangman game is under construction, maybe you'll get to play in a few weeks.." << endl;
+    cout << "This is what various stages of the hangman game will look like..." << endl;
+
+    cout << "Stage 0" << endl;
+
+    cout << "     |-----------------\n";
+    cout << "     |/         |\n";
+    cout << "     |\n";
+    cout << "     |\n";
+    cout << "     |\n";
+    cout << "     |\n";
+    cout << "     |\n";
+    cout << "===========\n";
+    
+}
